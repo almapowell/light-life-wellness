b725c6b9fc7565695943661e11129ac4b4bef003
almapowell <49799618+almapowell@users.noreply.github.com>
2020-06-12 14:44:45 -0600

    Updates

---
+++ precache-manifest.be72f4934cf13a9becc40ab7f94568df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31d56b62a72bb13647176433ffc8945",
+    "url": "/almapowell/light-life-wellness.git/index.html"
+  },
+  {
+    "revision": "0fe1ba491024b8eaa75a",
+    "url": "/almapowell/light-life-wellness.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe08b680a9d2650b9cba",
+    "url": "/almapowell/light-life-wellness.git/static/js/2.739124f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/almapowell/light-life-wellness.git/static/js/2.739124f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe1ba491024b8eaa75a",
+    "url": "/almapowell/light-life-wellness.git/static/js/main.4f8f3152.chunk.js"
+  },
+  {
+    "revision": "2bc13a533cf1f67c3650",
+    "url": "/almapowell/light-life-wellness.git/static/js/runtime-main.04701cdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/almapowell/light-life-wellness.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
