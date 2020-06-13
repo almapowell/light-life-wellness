ae57066713dbd3b844547e6433072b2ea36b23ca
almapowell <49799618+almapowell@users.noreply.github.com>
2020-06-13 02:11:41 -0700

    got the header functionality and styling complete

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
+import Contact from "./components/Contact";
 
 export default (
       <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
       </Switch>
   );
